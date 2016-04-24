11ce0382d3ca29ae2541e2b051487dc955960eb3
Ian Effendi <effendiian+github@gmail.com>
2016-04-24 17:54:00 -0400

    Overview: Laid out the basis for this project.

---
+++ PE22 - Dynamic Tree Creation/PE22 - Dynamic Tree Creation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE22___Dynamic_Tree_Creation
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		}
+	}
+}
